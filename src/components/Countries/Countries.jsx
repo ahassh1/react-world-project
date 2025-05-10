3f8a5672efbc2dc071978e857f2d059ef9a641ae
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-10 13:43:33 +0600

    use state in react and 3 colunm layout

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,16 +1,20 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countries = use(countriesPromise);
-    console.log(countries)
+    // console.log(countries)
 
     return (
         <div>
             <h1>travling Countries: {countries.length}</h1>
-            {
+         <div className='countries'>
+               {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
+               
             }
+         </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
-    console.log(country)
+     const handleVisited=()=>{
+        console.log('visited')
+     }
+    
     return (
-        <div>
+        <div className='country'>
             <h1>Name: {country?.name?.common}</h1>
             <img src={country.flags.png} alt="" />
             <p>Independent: {country.independent ? 'free' : 'not free'}</p>
             <h4>Populations: {country.population}</h4>
+            <button onClick={ handleVisited}> visited</button>
 
-        </div>
+        </div> 
     );
 };
 
